9b9687d243c05427d56f1e7cc80075cc50236369
pkgilles <67744351+pkgilles@users.noreply.github.com>
2026-07-24 12:30:39 +0200

    Fix restaurant data and popup content formatting
    Updated restaurant data to include latitude and longitude in the correct order. Adjusted popup content to display visit count.

--- Prosjekt_CPKM.py
+++ Prosjekt_CPKM.py
@@ -4,46 +4,48 @@ import folium
 from streamlit_folium import st_folium
 
 # Load the restaurant data
-
+# Breddegrad = latitude, Lengdegrad = longitude
 restaurants = [
-    {'Restaurant': 'Maaemo', 'Stjerner': 3, 'Lengdegrad': 59.907647183418746, 'Breddegrad': 10.75813998319409, 'Besøk': 2, 'Sum stjerner': 6},
-    {'Restaurant': 'Renaa', 'Stjerner': 2, 'Lengdegrad': 58.97396191894673, 'Breddegrad': 5.731084322652034, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Sketch', 'Stjerner': 2, 'Lengdegrad': 51.512721702120295, 'Breddegrad': -0.14144440934666339, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Kontrast', 'Stjerner': 2, 'Lengdegrad': 59.922982017962504, 'Breddegrad': 10.751167162303334, 'Besøk': 2, 'Sum stjerner': 3},
-    {'Restaurant': 'Chapter One', 'Stjerner': 1, 'Lengdegrad': 53.354313, 'Breddegrad': -6.2641001, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Middag Paris', 'Stjerner': 1, 'Lengdegrad': 48.86580095415907, 'Breddegrad': 2.3196169323413423, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Il Gallo D\'oro', 'Stjerner': 1, 'Lengdegrad': 32.63850818472831, 'Breddegrad': -16.926428884070905, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Restaurant Opus', 'Stjerner': 1, 'Lengdegrad': 48.20143884034287, 'Breddegrad': 16.372871288311156, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Disfrutar', 'Stjerner': 2, 'Lengdegrad': 41.38782363695626, 'Breddegrad': 2.153204200226883, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Alma', 'Stjerner': 2, 'Lengdegrad': 38.71047126916178, 'Breddegrad': -9.141003188669877, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Loco', 'Stjerner': 1, 'Lengdegrad': 38.712692233153376, 'Breddegrad': -9.160627528960182, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Birdland', 'Stjerner': 1, 'Lengdegrad': 35.67298943958676, 'Breddegrad': 139.76381215596643, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Fagn', 'Stjerner': 1, 'Lengdegrad': 63.434117339663665, 'Breddegrad': 10.396440642846406, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Statholdergaarden', 'Stjerner': 1, 'Lengdegrad': 59.909576312764315, 'Breddegrad': 10.743158646936871, 'Besøk': 2, 'Sum stjerner': 2},
-    {'Restaurant': 'La Degustation', 'Stjerner': 1, 'Lengdegrad': 50.091098220934434, 'Breddegrad': 14.425738457129478, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Adam/Albin', 'Stjerner': 1, 'Lengdegrad': 59.34310282115162, 'Breddegrad': 18.066249963979196, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Hyde', 'Stjerner': 1, 'Lengdegrad': 59.919678614026225, 'Breddegrad': 10.748774540864886, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Mon Oncle', 'Stjerner': 1, 'Lengdegrad': 59.91716932299051, 'Breddegrad': 10.738994821796187, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Ola (Martin Berasategui)', 'Stjerner': 1, 'Lengdegrad': 43.25810595552674, 'Breddegrad': -2.926402002033818, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Hakkasan Mayfair', 'Stjerner': 1, 'Lengdegrad': 51.51040318279479, 'Breddegrad': -0.14501570176840065, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Da Terra', 'Stjerner': 2, 'Lengdegrad': 51.53058708267891, 'Breddegrad': -0.05591802875661535, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'formel B', 'Stjerner': 1, 'Lengdegrad': 55.67091959080128, 'Breddegrad': 12.535634144400204, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Casona del Judio', 'Stjerner': 1, 'Lengdegrad': 43.46875599502872, 'Breddegrad': -3.828884023477654, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Arzak', 'Stjerner': 3, 'Lengdegrad': 43.321816318381906, 'Breddegrad': -1.9492931443615904, 'Besøk': 1, 'Sum stjerner': 3},
-    {'Restaurant': 'Savage', 'Stjerner': 1, 'Lengdegrad': 59.90965741447051, 'Breddegrad': 10.740274627370074, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Finnjävel Salonki', 'Stjerner': 1, 'Lengdegrad': 60.17210936992626, 'Breddegrad': 24.932450651381274, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Kitchen Table', 'Stjerner': 2, 'Lengdegrad': 51.52051886488125, 'Breddegrad': -0.13604940418496586, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Casa de Chá de Boa Nova', 'Stjerner': 2, 'Lengdegrad': 41.203092957202905, 'Breddegrad': -8.714882495958829, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Belcanto', 'Stjerner': 2, 'Lengdegrad': 38.71025006629505, 'Breddegrad': -9.141476330999446, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Jatak', 'Stjerner': 1, 'Lengdegrad': 55.6871699325071, 'Breddegrad': 12.548033871389647, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Ricard Camarena', 'Stjerner': 2, 'Lengdegrad': 39.48578556565042, 'Breddegrad': -0.38364874472450533, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Yugen', 'Stjerner': 2, 'Lengdegrad': 34.66494703388733, 'Breddegrad': 135.52174529818703, 'Besøk': 1, 'Sum stjerner': 2},
-    {'Restaurant': 'Velrosier', 'Stjerner': 1, 'Lengdegrad': 35.0026325147605, 'Breddegrad': 135.76882619820134, 'Besøk': 1, 'Sum stjerner': 1},
-    {'Restaurant': 'Florilege', 'Stjerner': 2, 'Lengdegrad': 35.66174874281927, 'Breddegrad': 139.74353242706522, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Maaemo', 'Stjerner': 3, 'Breddegrad': 59.907647183418746, 'Lengdegrad': 10.75813998319409, 'Besøk': 2, 'Sum stjerner': 6},
+    {'Restaurant': 'Renaa', 'Stjerner': 2, 'Breddegrad': 58.97396191894673, 'Lengdegrad': 5.731084322652034, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Sketch', 'Stjerner': 2, 'Breddegrad': 51.512721702120295, 'Lengdegrad': -0.14144440934666339, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Kontrast', 'Stjerner': 2, 'Breddegrad': 59.922982017962504, 'Lengdegrad': 10.751167162303334, 'Besøk': 2, 'Sum stjerner': 3},
+    {'Restaurant': 'Chapter One', 'Stjerner': 1, 'Breddegrad': 53.354313, 'Lengdegrad': -6.2641001, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Middag Paris', 'Stjerner': 1, 'Breddegrad': 48.86580095415907, 'Lengdegrad': 2.3196169323413423, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Il Gallo D\'oro', 'Stjerner': 1, 'Breddegrad': 32.63850818472831, 'Lengdegrad': -16.926428884070905, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Restaurant Opus', 'Stjerner': 1, 'Breddegrad': 48.20143884034287, 'Lengdegrad': 16.372871288311156, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Disfrutar', 'Stjerner': 2, 'Breddegrad': 41.38782363695626, 'Lengdegrad': 2.153204200226883, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Alma', 'Stjerner': 2, 'Breddegrad': 38.71047126916178, 'Lengdegrad': -9.141003188669877, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Loco', 'Stjerner': 1, 'Breddegrad': 38.712692233153376, 'Lengdegrad': -9.160627528960182, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Birdland', 'Stjerner': 1, 'Breddegrad': 35.67298943958676, 'Lengdegrad': 139.76381215596643, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Fagn', 'Stjerner': 1, 'Breddegrad': 63.434117339663665, 'Lengdegrad': 10.396440642846406, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Statholdergaarden', 'Stjerner': 1, 'Breddegrad': 59.909576312764315, 'Lengdegrad': 10.743158646936871, 'Besøk': 2, 'Sum stjerner': 2},
+    {'Restaurant': 'La Degustation', 'Stjerner': 1, 'Breddegrad': 50.091098220934434, 'Lengdegrad': 14.425738457129478, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Adam/Albin', 'Stjerner': 1, 'Breddegrad': 59.34310282115162, 'Lengdegrad': 18.066249963979196, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Hyde', 'Stjerner': 1, 'Breddegrad': 59.919678614026225, 'Lengdegrad': 10.748774540864886, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Mon Oncle', 'Stjerner': 1, 'Breddegrad': 59.91716932299051, 'Lengdegrad': 10.738994821796187, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Ola (Martin Berasategui)', 'Stjerner': 1, 'Breddegrad': 43.25810595552674, 'Lengdegrad': -2.926402002033818, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Hakkasan Mayfair', 'Stjerner': 1, 'Breddegrad': 51.51040318279479, 'Lengdegrad': -0.14501570176840065, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Da Terra', 'Stjerner': 2, 'Breddegrad': 51.53058708267891, 'Lengdegrad': -0.05591802875661535, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'formel B', 'Stjerner': 1, 'Breddegrad': 55.67091959080128, 'Lengdegrad': 12.535634144400204, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Casona del Judio', 'Stjerner': 1, 'Breddegrad': 43.46875599502872, 'Lengdegrad': -3.828884023477654, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Arzak', 'Stjerner': 3, 'Breddegrad': 43.321816318381906, 'Lengdegrad': -1.9492931443615904, 'Besøk': 1, 'Sum stjerner': 3},
+    {'Restaurant': 'Savage', 'Stjerner': 1, 'Breddegrad': 59.90965741447051, 'Lengdegrad': 10.740274627370074, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Finnjävel Salonki', 'Stjerner': 1, 'Breddegrad': 60.17210936992626, 'Lengdegrad': 24.932450651381274, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Kitchen Table', 'Stjerner': 2, 'Breddegrad': 51.52051886488125, 'Lengdegrad': -0.13604940418496586, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Casa de Chá de Boa Nova', 'Stjerner': 2, 'Breddegrad': 41.203092957202905, 'Lengdegrad': -8.714882495958829, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Belcanto', 'Stjerner': 2, 'Breddegrad': 38.71025006629505, 'Lengdegrad': -9.141476330999446, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Jatak', 'Stjerner': 1, 'Breddegrad': 55.6871699325071, 'Lengdegrad': 12.548033871389647, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Ricard Camarena', 'Stjerner': 2, 'Breddegrad': 39.48578556565042, 'Lengdegrad': -0.38364874472450533, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Yugen', 'Stjerner': 2, 'Breddegrad': 34.66494703388733, 'Lengdegrad': 135.52174529818703, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Velrosier', 'Stjerner': 1, 'Breddegrad': 35.0026325147605, 'Lengdegrad': 135.76882619820134, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'Florilege', 'Stjerner': 2, 'Breddegrad': 35.66174874281927, 'Lengdegrad': 139.74353242706522, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'Restaurant 212', 'Stjerner': 2, 'Breddegrad': 52.365882, 'Lengdegrad': 4.900279, 'Besøk': 1, 'Sum stjerner': 2},
+    {'Restaurant': 'À L\'aise', 'Stjerner': 1, 'Breddegrad': 59.929166, 'Lengdegrad': 10.709721, 'Besøk': 1, 'Sum stjerner': 1},
+    {'Restaurant': 'La Scène', 'Stjerner': 2, 'Breddegrad': 48.872063, 'Lengdegrad': 2.314564, 'Besøk': 1, 'Sum stjerner': 2},
 ]
 
 data = pd.DataFrame(restaurants)
-
 
 # Set up the Streamlit app
 st.markdown("<h1 style='text-align: center;'>Christina og Per Kristians Michelin-reise</h1>", unsafe_allow_html=True)
@@ -70,13 +72,13 @@ for idx, row in filtered_data.iterrows():
     popup_content = f"""
     <div style="width: 250px; font-size: 16px; background-color: #333333; color: #e0e0e0; padding: 10px; border-radius: 5px;">
         <strong>{row['Restaurant']}</strong><br>
-        Stjerner: {row['Stjerner']}
+        Stjerner: {row['Stjerner']}<br>
+        Besøk: {row['Besøk']}<br>
         Totalt: {row['Sum stjerner']}
     </div>
     """
-    
     folium.Marker(
-        location=[row['Lengdegrad'], row['Breddegrad']],
+        location=[row['Breddegrad'], row['Lengdegrad']],
         popup=folium.Popup(popup_content, max_width=300),
         icon=folium.Icon(icon="star", icon_color=marker_color, color="white", prefix="fa")  # Green star icon
     ).add_to(m)
